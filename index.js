8add783e09283c80ea58c45c2077b0436bf736fd
pandajiangbao <pandawsh@icloud.com>
2019-06-27 23:22:43 +0800

    finish homework

--- index.js
+++ index.js
@@ -4,13 +4,15 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
-
+console.log("'"+name.toUpperCase()+"'");
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+console.log(sentence.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase()));
 
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
 //TODO write your code here...
+console.log(money.substr(1,3));
